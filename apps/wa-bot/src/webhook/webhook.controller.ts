febd9c2e824738130cedc6b5a508c2ef48eeb5e9
Alfredo Paz <apaz20164920@gmail.com>
2026-02-11 23:03:48 -0600

    fix: configuracion de webhook

--- apps/wa-bot/src/webhook/webhook.controller.ts
+++ apps/wa-bot/src/webhook/webhook.controller.ts
@@ -1,13 +1,17 @@
 import { env } from "../config/env.js";
-import { Request, Response } from 'express';
-import { handleIncomingMessage, handleMessage } from "../handlers/message.handler.js";
+import { Request, Response } from "express";
+import {
+	handleIncomingMessage,
+	handleMessage,
+} from "../handlers/message.handler.js";
+import axios from "axios";
 
 export const whatsappWebhook = (req: Request, res: Response) => {
 	const mode = req.query["hub.mode"];
 	const token = req.query["hub.verify_token"];
 	const challenge = req.query["hub.challenge"];
 
-	if (mode === "subscribe" && token === env.waBotToken) {
+	if (mode === "subscribe" && token === env.whatsappWebHookVerifyToken) {
 		console.log("✅ Webhook verified");
 		return res.status(200).send(challenge);
 	}
@@ -17,9 +21,28 @@ export const whatsappWebhook = (req: Request, res: Response) => {
 
 export const receiveMessage = async (req: Request, res: Response) => {
 	try {
-		console.log(JSON.stringify(req.body, null, 2));
+		const entry = req.body.entry?.[0];
+		const changes = entry?.changes?.[0];
+		const message = changes?.value?.messages?.[0];
+
+		if (!message) {
+			return res.sendStatus(200);
+		}
+
+		const from = message.from;
+		const body = message.text?.body;
+
+		console.log("Mensaje recibido:", from, body);
+
+		// 🔹 Enviar a api-core
+		await axios.post(`${env.apiCoreUrl}/messages/incoming`, {
+			from,
+			body,
+		});
+
 		res.sendStatus(200);
-	} catch (error) {
-		console.error("❌ Error in receiveMessage:", error);
+	} catch (error: any) {
+		console.error("Error reenviando a api-core:", error.message);
+		res.sendStatus(500);
 	}
 };
